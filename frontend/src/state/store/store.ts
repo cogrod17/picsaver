d3df9a897ce1538474d2b6d48cd07829f8cf0e27
Cole Ogrodnick <patfan8@comcast.net>
2022-06-10 20:11:30 -0400

    setting up redux store :/

--- frontend/src/state/store/store.ts
+++ frontend/src/state/store/store.ts
@@ -1,19 +1,16 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { authSlice, userSlice } from "./slices";
+// import { applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 export const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     auth: authSlice.reducer,
   },
+  middleware: [thunk],
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-// const reducers = {
-//   auth: authSlice.reducer,
-//   user: userSlice.reducer,
-// };
-
-// export const appReducer = combineReducers(reducers);
