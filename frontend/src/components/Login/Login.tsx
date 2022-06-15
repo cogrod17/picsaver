bf4bebd3d27b9c9208ffe4e0c43f2ce872d63369
Cole Ogrodnick <patfan8@comcast.net>
2022-06-15 17:00:11 -0400

    lil bit of setup

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -22,8 +22,8 @@ export const Login = () => {
         render={({ handleSubmit }) => (
           <form className="login-form" onSubmit={handleSubmit}>
             <h1 className="login-header">Login</h1>
-            <Input label="username" name="username" type="text" />
-            <Input label="password" name="password" type="password" />
+            <Input label="Username" name="username" type="text" />
+            <Input label="Password" name="password" type="password" />
             <Button
               className="login-submit-button"
               text="login"
--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,3 +1,16 @@
+import "./Profile.scss";
+
+import { useAppSelector } from "hooks";
+import { userSelector } from "state";
+
 export const Profile = () => {
-  return <div>Profile</div>;
+  const { data } = useAppSelector(userSelector);
+
+  return (
+    <div className="profile-wrap">
+      <div className="profile-head">{data?.username}</div>
+      <div className="profile-slider"></div>
+      <div className="profile-content"></div>
+    </div>
+  );
 };
